e087cad5671140c7b90030adfa3f8cc952db769f
Black Roland <mail@roland.black>
2024-10-06 01:57:33 +0300

    yandexgpt_sensor -> yandexgpt_conversation

--- custom_components/yandexgpt_conversation/const.py
+++ custom_components/yandexgpt_conversation/const.py
@@ -6,19 +6,16 @@
 
 import logging
 
-DOMAIN = "yandexgpt_sensor"
+DOMAIN = "yandexgpt_conversation"
 LOGGER = logging.getLogger(__package__)
 
 CONF_CATALOG_ID = "catalog_id"
 CONF_API_KEY = "api_key"
 CONF_MODEL_TYPE = "model_type"
-
 DEFAULT_MODEL = "yandexgpt-lite"
 
 CONF_PROMPT = "prompt"
-
 CONF_RECOMMENDED = "recommended"
-
 CONF_MAX_TOKENS = "max_tokens"
 RECOMMENDED_MAX_TOKENS = 1024
 CONF_TEMPERATURE = "temperature"
