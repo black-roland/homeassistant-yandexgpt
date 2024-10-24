f724e8bfb6415068af4047d4af5da2b2aa0d2796
Black Roland <mail@roland.black>
2024-10-24 19:27:03 +0300

    YandexART refactoring

--- custom_components/yandexgpt_conversation/__init__.py
+++ custom_components/yandexgpt_conversation/__init__.py
@@ -6,18 +6,22 @@
 
 from __future__ import annotations
 
-import asyncio
 import base64
 
 import voluptuous as vol
-from async_upnp_client import aiohttp
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform, CONF_API_KEY
-from homeassistant.core import HomeAssistant, SupportsResponse, ServiceCall, ServiceResponse
+from homeassistant.core import (
+    HomeAssistant,
+    SupportsResponse,
+    ServiceCall,
+    ServiceResponse,
+)
 from homeassistant.helpers import config_validation as cv, selector
 from homeassistant.helpers.typing import ConfigType
 from yandex_cloud_ml_sdk import YCloudML
 
+from .yandexart import YandexArt
 from .const import DOMAIN, CONF_FOLDER_ID
 
 SERVICE_GENERATE_IMAGE = "generate_image"
@@ -25,12 +29,11 @@ PLATFORMS = (Platform.CONVERSATION,)
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    def write_image_to_file(base64_img, file_name):
+    def write_image_to_file(base64_img: str, file_name: str) -> str:
         with open(file_name, "wb") as file:
             file.write(base64.b64decode(base64_img))
         return file_name
 
-    # FIXME: Needs refactoring
     async def render_image(call: ServiceCall) -> ServiceResponse:
         """Render an image with YandexART."""
 
@@ -38,66 +41,16 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
         entry = hass.config_entries.async_get_entry(entry_id)
 
         settings = {**entry.data, **entry.options}
+        yandexart = YandexArt(
+            folder_id=settings[CONF_FOLDER_ID], api_key=settings[CONF_API_KEY]
+        )
+        base64_img = await yandexart.generate(call.data["seed"], call.data["prompt"])
 
-        payload = {
-            "modelUri": f"art://{settings[CONF_FOLDER_ID]}/yandex-art/latest",
-            "generationOptions": {
-                "seed": call.data["seed"],
-                "aspectRatio": {
-                    "widthRatio": "16",
-                    "heightRatio": "9",
-                },
-            },
-            "messages": [
-                {
-                    "weight": "1",
-                    "text": call.data["prompt"],
-                }
-            ],
-        }
+        file_name = await hass.async_add_executor_job(
+            write_image_to_file, base64_img, call.data["file_name"]
+        )
 
-        headers = {
-            "Accept": "application/json",
-            "Authorization": f"Api-Key {settings[CONF_API_KEY]}"
-        }
-
-        operation_id = None
-
-        async with aiohttp.ClientSession() as session:
-            async with session.post("https://llm.api.cloud.yandex.net/foundationModels/v1/imageGenerationAsync",
-                                    headers=headers, json=payload) as resp:
-                if resp.status == 200:
-                    data = await resp.json()
-                    operation_id = data["id"]
-                else:
-                    data = await resp.text()
-                    raise Exception(f"Failed to send async request, status code: {resp.status}")
-
-            base64_img = None
-
-            async with aiohttp.ClientSession() as session:
-                end_time = asyncio.get_event_loop().time() + 30
-                while True:
-                    # Check if the operation has timed out and if so, raise an exception
-                    if asyncio.get_event_loop().time() > end_time:
-                        raise TimeoutError("Operation timed out after 30 seconds")
-                    # Polling the operation
-                    async with session.get(f"https://llm.api.cloud.yandex.net/operations/{operation_id}",
-                                           headers=headers) as resp:
-                        # If the request was successful, return the completion result
-                        # Otherwise, raise an exception
-                        if resp.status == 200:
-                            data = await resp.json()
-                            if data.get('done', False):
-                                base64_img = data["response"]["image"]
-                                break
-                        else:
-                            raise Exception(f"Failed to poll operation status, status code: {resp.status}")
-                    await asyncio.sleep(1)
-
-            file_name = await hass.async_add_executor_job(write_image_to_file, base64_img, call.data["file_name"])
-
-            return {"file_name": file_name}
+        return {"file_name": file_name}
 
     hass.services.async_register(
         DOMAIN,
@@ -110,9 +63,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
                         "integration": DOMAIN,
                     }
                 ),
-                vol.Required("seed"): vol.All(
-                    vol.Coerce(int), vol.Clamp(min=0)
-                ),
+                vol.Required("seed"): vol.All(vol.Coerce(int), vol.Clamp(min=0)),
                 vol.Required("prompt"): cv.string,
                 vol.Required("file_name"): cv.path,
             }
@@ -127,7 +78,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up YandexGPT from a config entry."""
     settings = {**entry.data, **entry.options}
 
-    yandexgpt_sdk = YCloudML(folder_id=settings[CONF_FOLDER_ID], auth=settings[CONF_API_KEY])
+    yandexgpt_sdk = YCloudML(
+        folder_id=settings[CONF_FOLDER_ID], auth=settings[CONF_API_KEY]
+    )
 
     entry.runtime_data = yandexgpt_sdk
 
---
+++ custom_components/yandexgpt_conversation/yandexart.py
@@ -0,0 +1,83 @@
+"""YandexART wrapper."""
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import asyncio
+import aiohttp
+
+
+class YandexArt:
+    """YandexART wrapper."""
+
+    def __init__(self, folder_id, api_key):
+        self.folder_id = folder_id
+        self.api_key = api_key
+
+    async def generate(self, seed, prompt):
+
+        operation_id = None
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                "https://llm.api.cloud.yandex.net/foundationModels/v1/imageGenerationAsync",
+                headers=self._request_headers,
+                json=self._build_request_payload(seed, prompt),
+            ) as resp:
+                if resp.status == 200:
+                    data = await resp.json()
+                    operation_id = data["id"]
+                else:
+                    data = await resp.text()
+                    raise Exception(
+                        f"Failed to send async request, status code: {resp.status}"
+                    )
+
+            return await self._poll_for_results(self._request_headers, operation_id)
+
+    def _build_request_payload(self, seed, prompt):
+        return {
+            "modelUri": f"art://{self.folder_id}/yandex-art/latest",
+            "generationOptions": {
+                "seed": seed,
+                "aspectRatio": {
+                    "widthRatio": "16",
+                    "heightRatio": "9",
+                },
+            },
+            "messages": [
+                {
+                    "weight": "1",
+                    "text": prompt,
+                }
+            ],
+        }
+
+    @property
+    def _request_headers(self):
+        return {
+            "Accept": "application/json",
+            "Authorization": f"Api-Key {self.api_key}",
+        }
+
+    async def _poll_for_results(self, headers, operation_id):
+        async with aiohttp.ClientSession() as session:
+            end_time = asyncio.get_event_loop().time() + 30
+            while True:
+                if asyncio.get_event_loop().time() > end_time:
+                    raise TimeoutError("Operation timed out after 30 seconds")
+
+                async with session.get(
+                    f"https://llm.api.cloud.yandex.net/operations/{operation_id}",
+                    headers=headers,
+                ) as resp:
+                    if resp.status == 200:
+                        data = await resp.json()
+                        if data.get("done", False):
+                            return data["response"]["image"]
+                    else:
+                        raise Exception(
+                            f"Failed to poll operation status, status code: {resp.status}"
+                        )
+                await asyncio.sleep(1)
