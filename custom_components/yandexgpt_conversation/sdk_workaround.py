8ed62c368c750f0e9b4b2c111d3f75aafe7b346c
Black Roland <mail@roland.black>
2025-08-29 18:45:52 +0300

    Use default constraints for the Protobuf version workaround

--- custom_components/yandexgpt_conversation/sdk_workaround.py
+++ custom_components/yandexgpt_conversation/sdk_workaround.py
@@ -4,6 +4,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+import asyncio
+
+from homeassistant.bootstrap import async_mount_local_lib_path
+from homeassistant.config import get_default_config_dir
+from homeassistant.requirements import pip_kwargs
 from homeassistant.util import package as pkg_util
 from .const import LOGGER
 
@@ -37,21 +42,42 @@ def is_latest_sdk_installed() -> bool:
 def upgrade_sdk() -> bool:
     """Use pkg_util to install the required versions of the SDKs."""
 
-    # Empty args to avoid setting any package constraints
-    _pip_kwargs = {}
+    LOGGER.warning(
+        "Этой интеграции требуется старая версия Protobuf из-за чего могут "
+        "возникать конфликты с другими компонентами Home Assistant.\n"
+        "Иногда это может влиять на работу других интеграций (например, ESPHome).\n"
+        "Это ограничение Yandex Cloud SDK. Для постоянного решения команде "
+        "Yandex Cloud SDK необходимо добавить поддержку Protobuf 6.x. "
+        "Пожалуйста, поддержите issue на GitHub, чтобы ускорить исправление:\n"
+        "https://github.com/yandex-cloud/python-sdk/issues/151"
+    )
+
+    # Get config directory
+    config_dir = get_default_config_dir()
+
+    # Mount local lib path if not in a virtual environment
+    if not pkg_util.is_virtual_env():
+        asyncio.run(async_mount_local_lib_path(config_dir))
+
+    # Get pip kwargs
+    _pip_kwargs = pip_kwargs(config_dir)
 
     success = True
     for package in REQUIRED_PACKAGES:
         try:
-            LOGGER.info("Attempting to install/upgrade Yandex Cloud SDK package: %s", package)
+            LOGGER.info("Attempting to install/upgrade YandexGPT dependency: %s", package)
 
-            install_success = pkg_util.install_package(package, **_pip_kwargs)
+            # Use empty kwargs for yandex packages, otherwise use pip_kwargs
+            package_kwargs = {} if "yandex" in package else _pip_kwargs
+            LOGGER.debug("Contraints for package %s: %s", package, package_kwargs.get("constraints", "None"))
+
+            install_success = pkg_util.install_package(package, **package_kwargs)
             if not install_success:
-                LOGGER.error("Failed to install/upgrade Yandex Cloud SDK package: %s", package)
+                LOGGER.error("Failed to install/upgrade YandexGPT dependency: %s", package)
                 success = False
                 continue
 
-            LOGGER.info("Successfully installed/upgraded Yandex Cloud SDK package: %s", package)
+            LOGGER.info("Successfully installed/upgraded YandexGPT dependency: %s", package)
         except Exception as e:
             LOGGER.exception("Exception occurred while installing/upgrading package %s: %s", package, e)
             success = False
